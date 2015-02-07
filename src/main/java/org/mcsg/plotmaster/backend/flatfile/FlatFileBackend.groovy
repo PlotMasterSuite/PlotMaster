ac6c1b6a978cd3f472bd88a3d3c14b7c4af768cd
Double0negative <Double0negative@mc-sg.org>
2015-02-07 12:30:54 -0500

    implement FlatFile Loading and saving

--- src/main/java/org/mcsg/plotmaster/backend/flatfile/FlatFileBackend.groovy
+++ src/main/java/org/mcsg/plotmaster/backend/flatfile/FlatFileBackend.groovy
@@ -1,6 +1,9 @@
 package org.mcsg.plotmaster.backend.flatfile
 
+import com.google.gson.Gson
+
 import groovy.lang.Closure;
+import groovy.transform.CompileStatic;
 
 import org.mcsg.plotmaster.Plot;
 import org.mcsg.plotmaster.PlotMaster;
@@ -20,12 +23,18 @@ class FlatFileBackend implements Backend{
 	File userFolder
 
 
-	Map regionMap
-	Map plotMap
+	Map<Integer, XZLoc> regionMap
+	Map<Integer, Integer> plotMap
 
+	Gson gson = new Gson();
+
+	public class XZLoc {
+		int x, z
+	}
 
 	public void load(String world) {
 		File loc = new File(PlotMaster.getPlugin().getDataFolder(), Settings.config.backend.flatfile.location);
+
 		if(!loc.exists())
 			loc.mkdirs()
 
@@ -36,36 +45,59 @@ class FlatFileBackend implements Backend{
 		regionMapFile = new File(folder, "regionmap.json")
 		regionMapFile.createNewFile()
 
+		regionMap = gson.fromJson(regionMapFile.getText(), Map.class)
+
 		plotMapFile = new File(folder, "plotmap.json")
 		plotMapFile.createNewFile()
-	}
 
-	public void getRegion(int id) {
-		// TODO Auto-generated method stub
+		plotMap = gson.fromJson(regionMapFile.getText(), Map.class)
 
 	}
 
-	public void getRegionByLocation(int x, int z) {
-		// TODO Auto-generated method stub
+	public Region getRegion(int id) {
+		XZLoc loc = regionMap.get(id.toString())
 
+		if(loc == null){
+			return null
+		}
+
+		return getRegionByLocation(loc.x, id)
+	}
+
+	public Region getRegionByLocation(int x, int z) {
+		def file = new File(regionFolder, "${x}.${z}.rg")
+
+		if(!file)
+			return null
+
+		gson.fromJson(file.getText(), Region.class)
 	}
 
 	public void saveRegion(Region region) {
+		assert region != null, "Region cannot be null"
+		
+		def file = new File(regionFolder, "${region.x}.${region.z}.rg")
+		file.createNewFile()
+		file.setText(gson.toJson(region))
+	}
+
+	public Plot getPlot(int id) {
+		XZLoc loc = plotMap.get(id.toString())
+		
+		if(!loc)
+			return null
+			
+		Region region = getRegionByLocation(loc.x, loc.z)
+		
+		return region.plots.get(id)
+	}
+
+	public Region createRegion(int x, int y) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void getPlot(int id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void createRegion(int x, int y) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void createPlot(Region region, int x, int y, PlotType type) {
+	public Plot createPlot(Region region, int x, int y, PlotType type) {
 		// TODO Auto-generated method stub
 
 	}
