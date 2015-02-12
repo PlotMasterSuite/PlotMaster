d24cc933c3a814a95fa5c1f017693c0b0fe7b2ed
Double0negative <Double0negative@mc-sg.org>
2015-02-12 14:05:01 -0500

    Add Bukkit Plugin

---
+++ src/main/java/bukkit/org/mcsg/plotmaster/PlotMasterPlugin.java
@@ -0,0 +1,52 @@
+package bukkit.org.mcsg.plotmaster;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mcsg.plotmaster.PlotMaster;
+
+public class PlotMasterPlugin extends JavaPlugin{
+
+	private static PlotMasterPlugin plugin;
+	
+	public void onEnable(){
+		PlotMaster plotMaster = null;
+		try{
+			plotMaster = new PlotMaster();
+		} catch (Exception e){
+			e.printStackTrace();
+			
+			sendConsoleMessage(ChatColor.RED+"**********************************************************");
+			sendConsoleMessage("");
+			sendConsoleMessage(ChatColor.GOLD+"  GroovyRuntimeSupport is required to run this plugin!");
+			sendConsoleMessage(ChatColor.GOLD+"  Download: https://ci.drtshock.net/view/Double0negative/");
+			sendConsoleMessage("");
+			sendConsoleMessage(ChatColor.RED+"**********************************************************");
+			
+			return;
+	
+		}
+		
+		plotMaster.onEnable(this);
+		
+	}
+	
+	
+	public void onDisable(){
+		
+	}
+	
+	public static PlotMasterPlugin getPlugin(){
+		return plugin;
+	}
+	
+	private void sendConsoleMessage(String message){
+		Bukkit.getConsoleSender().sendMessage(message);
+	}
+	
+	
+
+	
+	
+}
