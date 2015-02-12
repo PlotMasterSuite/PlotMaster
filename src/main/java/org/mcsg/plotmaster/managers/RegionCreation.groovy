5fa3b200c757ac5f777173a86d007cb92ed91a24
givemeapples <givemeapples@users.noreply.github.com>
2015-02-11 19:55:29 -0500

    Update RegionCreation.groovy
    no camel case!

--- src/main/java/org/mcsg/plotmaster/managers/RegionCreation.groovy
+++ src/main/java/org/mcsg/plotmaster/managers/RegionCreation.groovy
@@ -5,7 +5,7 @@ import org.mcsg.plotmaster.Region
 class RegionCreation {
 
 	enum RegionCreationStatus {
-		SUCCESS("Region Created Successfully."),
+		SUCCESS("Region created successfully."),
 		REGION_EXISTS("A region already exists in this location!"),
 		OTHER("Failed to create region.")
 
