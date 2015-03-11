3007205cd0a5d76f5caa6abe7b1c5707d5b26631
Double0negative <Double0negative@mc-sg.org>
2015-03-11 00:48:11 -0400

    Add reset command

---
+++ src/main/java/bukkit/org/mcsg/plotmaster/listeners/BlockListeners.groovy
@@ -0,0 +1,16 @@
+package bukkit.org.mcsg.plotmaster.listeners
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent;
+
+class BlockListeners implements Listener{
+
+	
+	@EventHandler
+	public void place(BlockPlaceEvent e){
+		
+	}
+	
+	
+}
---
+++ src/main/java/org/mcsg/plotmaster/command/commands/sub/ResetSubCommand.groovy
@@ -0,0 +1,32 @@
+package org.mcsg.plotmaster.command.commands.sub
+
+import java.util.List;
+
+import org.mcsg.plotmaster.Plot
+import org.mcsg.plotmaster.PlotMaster;
+import org.mcsg.plotmaster.bridge.PMPlayer;
+import org.mcsg.plotmaster.command.PlayerSubCommand;
+import org.mcsg.plotmaster.managers.PlotManager
+
+class ResetSubCommand implements PlayerSubCommand {
+
+	@Override
+	public String help() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCommand(PMPlayer player, List<String> args) {
+		
+		PlotManager manager = PlotMaster.getInstance().getManager(player.getLocation().getWorld())
+		Plot p = manager.getPlot(player.getLocation().getX(), player.getLocation().getZ(), null)
+		
+		p.reset(manager.getSettings()) {
+			player.sendMessage("&aPlot has been cleared!")
+		}
+		
+		
+	}
+
+}
