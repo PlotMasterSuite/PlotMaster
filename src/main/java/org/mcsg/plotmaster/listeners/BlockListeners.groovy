8863dc3acd5f9c34c23c969770b245827910e13a
Double0negative <Double0negative@mc-sg.org>
2015-03-11 18:29:21 -0400

    Block placing/breaking

--- src/main/java/org/mcsg/plotmaster/listeners/BlockListeners.groovy
+++ src/main/java/org/mcsg/plotmaster/listeners/BlockListeners.groovy
@@ -1,20 +1,35 @@
 package org.mcsg.plotmaster.listeners
 
+import org.mcsg.plotmaster.PlotMaster;
 import org.mcsg.plotmaster.bridge.PMBlock
 import org.mcsg.plotmaster.bridge.PMLocation
 import org.mcsg.plotmaster.bridge.PMPlayer
 
 class BlockListeners {
 
-	
+
 	static boolean blockPlace(PMPlayer player, PMLocation location, PMBlock block){
-		
+		def man = PlotMaster.getInstance().getManager(location.getWorld())
+		def bol = man.canModifyLocation(player, location)
+
+		if(!bol) {
+			player.sendMessage("&aCannot modify this location!")
+		}
+
+		return !bol
 	}
-	
+
 	static boolean blockBreak(PMPlayer player, PMLocation location, PMBlock block){
-		
+		def man = PlotMaster.getInstance().getManager(location.getWorld())
+		def bol = man.canModifyLocation(player, location)
+
+		if(!bol) {
+			player.sendMessage("&aCannot modify this location!")
+		}
+
+		return !bol
 	}
-	
-	
-	
+
+
+
 }
