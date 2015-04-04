dbb8f218ec2e0aff1c2e98f6a6babec2348977b8
Double0negative <Double0negative@mc-sg.org>
2015-04-04 17:34:59 -0400

    Add help subcommand

--- src/main/java/org/mcsg/plotmaster/command/commands/sub/HelpSubCommand.groovy
+++ src/main/java/org/mcsg/plotmaster/command/commands/sub/HelpSubCommand.groovy
@@ -6,6 +6,7 @@ import java.util.Map.Entry
 import org.mcsg.plotmaster.bridge.PMCommandSender;
 import org.mcsg.plotmaster.command.ConsoleSubCommand;
 import org.mcsg.plotmaster.command.RootCommand
+import org.mcsg.plotmaster.command.SubCommand
 
 class HelpSubCommand implements ConsoleSubCommand{
 
@@ -25,19 +26,21 @@ class HelpSubCommand implements ConsoleSubCommand{
 
 	public boolean onCommand(PMCommandSender player, List<String> args) {
 		def subs = command.getSubs()
+		def coms = new HashSet(subs.values())
 		
 		
 		
-		subs.entrySet().each { Entry entry ->
-			player.sendMessage("&6/${root} ${entry.getKey()} 
+		coms.each { SubCommand com ->
+			player.sendMessage("&6/${root} ${com.getCommand()}: &7 ${com.help()} ")
 		}
-		
-		
-		
-		
-		
-		
 		return false;
 	}
+
+
+
+	public String getCommand() {
+		return "help";
+	}
+	
 	
 }
