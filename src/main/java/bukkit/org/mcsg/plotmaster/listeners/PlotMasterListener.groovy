f306f394d40de6160f7c7689bc198e453cf3bc3e
Double0negative <Double0negative@mc-sg.org>
2015-04-05 16:55:30 -0400

    Fix respawning entities

--- src/main/java/bukkit/org/mcsg/plotmaster/listeners/PlotMasterListener.groovy
+++ src/main/java/bukkit/org/mcsg/plotmaster/listeners/PlotMasterListener.groovy
@@ -8,27 +8,37 @@ import bukkit.org.mcsg.plotmaster.util.EntityData
 import org.bukkit.Bukkit;
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player
 import org.mcsg.plotmaster.Plot
 import org.mcsg.plotmaster.events.PlayerEnterPlotEvent
 import org.mcsg.plotmaster.events.PlotCreationEvent;
 import org.mcsg.plotmaster.events.PlotOfflineEvent;
 import org.mcsg.plotmaster.events.PlotOnlineEvent;
+import org.mcsg.plotmaster.utils.SchedulerAdapter;
 
 import com.google.common.eventbus.Subscribe;
 
 @CompileStatic
 class PlotMasterListener {
-
+	
 	@Subscribe
 	public void PlotLoad(PlotOnlineEvent e) {
 		Plot plot = e.getPlot()
 		
 		if(plot.getSetting("save-entities") as boolean) {
-			e.getPlot().getMetaData("entites")?.each { EntityData data ->
-			
-				Location loc = new Location(Bukkit.getWorld(plot.getWorld()), data.x, data.y, data.z)
-				loc.getWorld().spawnEntity(loc, data.type)
+			SchedulerAdapter.delayed(0) {
+				e.getPlot().getMetaData("entities")?.each { EntityData data ->
+					try {
+						Location loc = new Location(Bukkit.getWorld(plot.getWorld()), data.x, data.y, data.z)
+						loc.getWorld().spawnEntity(loc, data.type)
+						
+					}catch (r) {
+						r.printStackTrace();
+					}
+					println "Spawning ${data.type}"
 					
+				}
 			}
 		}
 	}
@@ -47,15 +57,17 @@ class PlotMasterListener {
 			ents.each {
 				def loc = it.getLocation()
 				if(plot.isPartOf(loc.getBlockX(), loc.getBlockZ())) {
-					if(save) {
-						savelist.add(new EntityData(type: it.getType(), x: loc.getBlockX(), y: loc.getBlockY(), z: loc.getBlockZ()))
+					if(!(it instanceof Player)) {
+						if(save) {
+							savelist.add(new EntityData(type: it.getType(), x: loc.getBlockX(), y: loc.getBlockY(), z: loc.getBlockZ()))
+						}
+						it.remove()
 					}
-					it.remove()
 				}
 			}
 			
 			if(save){
-				plot.setMetaData("entities", savelist)
+				plot.setMetaData("entities", savelist).save()
 			}
 		}
 	}
